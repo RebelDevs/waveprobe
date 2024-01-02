ed00359f751a359dcc72371a12198b257436f7f8
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2024-01-02 15:55:47 +0000

    test: posix cmd execution

---
+++ probe/bin/mocks/ping.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Initialize ARGS as an empty array
+ARGS=()
+
+# Populate ARGS with all arguments starting from the second, if any
+if [ $# -gt 0 ]; then
+    ARGS=( "${@:1}" )
+fi
+
+# Handling the case when ARGS is empty
+if [ ${#ARGS[@]} -eq 0 ]; then
+    TARGET=""
+else
+    # Get the last element of the ARGS array
+    TARGET=${ARGS[${#ARGS[@]}-1]}
+fi
+
+if [ "$TARGET" = "error" ]; then
+  >&2 echo "error"
+  exit 1;
+fi
+
+echo "PING google.com (142.251.129.142) 56(84) bytes of data.
+64 bytes from gru14s31-in-f14.1e100.net (142.251.129.142): icmp_seq=1 ttl=60 time=3.34 ms
+64 bytes from gru14s31-in-f14.1e100.net (142.251.129.142): icmp_seq=2 ttl=60 time=3.35 ms
+64 bytes from gru14s31-in-f14.1e100.net (142.251.129.142): icmp_seq=3 ttl=60 time=3.38 ms
+
+--- google.com ping statistics ---
+3 packets transmitted, 3 received, 0% packet loss, time 401ms
+rtt min/avg/max/mdev = 3.339/3.358/3.384/0.019 ms"
+exit 0;
--- probe/src/commands/utils/posix.rs
+++ probe/src/commands/utils/posix.rs
@@ -1,10 +1,7 @@
-use std::process::Command;
+use std::process::{Command, Output};
 
 pub fn run(script: String, args: Vec<String>) -> Result<String, String> {
-    let process = Command::new(script)
-        .args(args)
-        .output()
-        .expect("failed to start command");
+    let process = execute(script, args);
 
     let stdres = if process.status.success() {
         process.stdout
@@ -15,14 +12,47 @@ pub fn run(script: String, args: Vec<String>) -> Result<String, String> {
     match std::str::from_utf8(&stdres) {
         Ok(str) => {
             if process.status.success() {
-                Ok(str.to_string())
+                Ok(str.trim().to_string())
             } else {
-                Err(str.to_string())
+                Err(str.trim().to_string())
             }
         }
         Err(e) => {
             eprintln!("failed to parse result, {}", e);
             Err("Internal error".to_string())
         }
+    }
+}
+
+fn execute(script: String, args: Vec<String>) -> Output {
+    return Command::new(script)
+        .args(args)
+        .output()
+        .expect("failed to start command");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn success() {
+        let result = run(
+            "./bin/mocks/ping.sh".to_string(),
+            vec!["-c 4".to_string(), "success".to_string()],
+        );
+
+        assert!(result.is_ok());
+        assert!(result.unwrap().starts_with("PING"));
+    }
+
+    #[test]
+    fn fail() {
+        let result = run(
+            "./bin/mocks/ping.sh".to_string(),
+            vec!["-c 4".to_string(), "error".to_string()],
+        );
+
+        assert_eq!(result, Err("error".to_string()));
     }
 }
