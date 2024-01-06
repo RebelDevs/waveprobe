04b3b7a851f398fa596583144a2d63382e2b8e5c
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2024-01-06 14:49:45 +0000

    fix: probe shuts down after startup

--- probe/src/main.rs
+++ probe/src/main.rs
@@ -2,6 +2,7 @@
 
 extern crate dotenv;
 use dotenv::dotenv;
+use tokio::task;
 
 mod commands;
 mod queue;
@@ -11,26 +12,35 @@ use queue::handlers::command_execute;
 async fn main() {
     dotenv().ok();
 
-    queue::connection::init().await;
+    let queue_init = task::spawn(async {
+        queue::connection::init().await;
+    });
 
-    let options = commands::ping::ping::Options {
-        hostname: "google.com".to_string(),
-        packets: 4,
-    };
-    let command = command_execute::handler::CommandRequest {
-        command: String::from("ping"),
-        id: String::from("123"),
-        options,
-    };
+    task::spawn(async {
+        let options = commands::ping::ping::Options {
+            hostname: "google.com".to_string(),
+            packets: 4,
+        };
+        let command = command_execute::handler::CommandRequest {
+            command: String::from("ping"),
+            id: String::from("123"),
+            options,
+        };
 
-    match serde_json::to_string(&command) {
-        Ok(json) => {
-            let result = command_execute::handler::handle(json.as_bytes());
-            println!("{:?}", json);
-            println!("{:?}", result);
+        match serde_json::to_string(&command) {
+            Ok(json) => {
+                let result = command_execute::handler::handle(json.as_bytes());
+                println!("{:?}", json);
+                println!("{:?}", result);
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
         }
-        Err(e) => {
-            println!("{}", e);
-        }
+    });
+
+    match queue_init.await {
+        Ok(_) => println!("mqtt down"),
+        Err(e) => eprintln!("{}", e),
     }
 }
